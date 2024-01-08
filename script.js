70cb7be3c8d56633855a0c35c46f2a77b30b2a12
trayhard0 <119894377+trayhard0@users.noreply.github.com>
2024-01-08 18:21:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,81 +1,65 @@
+// Hello :)
 const grid_container = document.querySelector(".grid_of_buttons");
-
 const mines_element = document.getElementById('mines');
 const timer_element = document.getElementById('timer');
 const restart_element = document.getElementById("restart");
-
 const ORIGINAL_number_of_bombs=40;
 let number_of_bombs = 40;
 let displayed_number_of_bombs = 40;
-
 let grid_of_bombs = [];
 let rows = 16;
 let columns = 16;
 let game_running = true;
 
-
-restart_element.addEventListener('click',()=>
+create_grid();
+function create_grid()
 {
-  console.log("clicked");
-})
-
-
-
-for(let x=0;x<rows;x++)
-{
-  grid_of_bombs[x]=[];
-  for(let y=0;y<columns;y++)
+  for(let x=0;x<rows;x++)
   {
-    grid_of_bombs[x][y]= 0;
-  }
-}
-
-
-while(number_of_bombs>0)
-{
-  let row = Math.floor(Math.random()*16);
-  let column = Math.floor(Math.random()*16);
-  if(grid_of_bombs[row][column] !== -1)
-  {
-    grid_of_bombs[row][column]= -1;
-    number_of_bombs--;
-  }
-  else
-  {
-    //console.log("failure");
-  }
-}
-
-for(let x=0;x<rows;x++)
-{
-  for(let y=0;y<columns;y++)
-  {
-    if(grid_of_bombs[x][y] != -1)
+    grid_of_bombs[x]=[];
+    for(let y=0;y<columns;y++)
     {
-      let counter=0;
-      for(let i=-1;i<=1;i++)
+      grid_of_bombs[x][y]= 0;
+    }
+  }
+  while(number_of_bombs>0)
+  {
+    let row = Math.floor(Math.random()*16);
+    let column = Math.floor(Math.random()*16);
+    if(grid_of_bombs[row][column] !== -1)
+    {
+      grid_of_bombs[row][column]= -1;
+      number_of_bombs--;
+    }
+  }
+  for(let x=0;x<rows;x++)
+  {
+    for(let y=0;y<columns;y++)
+    {
+      if(grid_of_bombs[x][y] != -1)
       {
-        for(let k=-1;k<=1;k++)
+        let counter=0;
+        for(let i=-1;i<=1;i++)
         {
-          if(x+i<rows && x+i>=0 && y+k<columns && y+k>=0 && grid_of_bombs[x+i][y+k] == -1)
+          for(let k=-1;k<=1;k++)
           {
-            counter++;
+            if(x+i<rows && x+i>=0 && y+k<columns && y+k>=0 && grid_of_bombs[x+i][y+k] == -1)
+            {
+              counter++;
+            }
           }
         }
+        grid_of_bombs[x][y]=counter;
       }
-      grid_of_bombs[x][y]=counter;
     }
   }
 }
-//console.log(grid_of_bombs);
-
 for(let x=0;x<16;x++)
 {
   for(let y=0;y<16;y++)
   {
     const button = document.createElement("button");
     button.classList.add("grid_button");
-
     button.addEventListener('click',()=>
     {
       if(game_running==true)
@@ -84,7 +68,6 @@ for(let x=0;x<16;x++)
         check_winner();
       }
     })
-
     button.addEventListener("contextmenu",function(event)
     {
       event.preventDefault();
@@ -112,7 +95,6 @@ for(let x=0;x<16;x++)
           {
             mines_element.textContent=displayed_number_of_bombs;
           }
-          
         }
         else if(!button.disabled)
         {
@@ -133,14 +115,9 @@ for(let x=0;x<16;x++)
         }
       }
     })
-    
     grid_container.appendChild(button);
   }
 }
-
-
-
-
 function click(button,x,y)
 {
   if(grid_of_bombs[x][y]>0 && button.querySelector('img') == null)
@@ -159,18 +136,15 @@ function click(button,x,y)
       button.style.color="purple";
     if(grid_of_bombs[x][y] >=5)
       button.style.color="orange";
-
     button.disabled=true;
     button.textContent=grid_of_bombs[x][y];
   }
-  
   if(grid_of_bombs[x][y]==0 && !button.disabled && button.querySelector('img') == null)
   {
     button.style.border = '1px solid black';
     button.style.fontWeight = 'bold';
     button.style.backgroundColor = "gray";
     button.disabled=true;
-
     for(let i=-1;i<=1;i++)
     {
       for(let k=-1;k<=1;k++)
@@ -182,7 +156,6 @@ function click(button,x,y)
       }
     }
   }
-
   if(grid_of_bombs[x][y]==-1 && button.querySelector("img") == null)
   {
     restart_element.textContent=":(";
@@ -190,7 +163,6 @@ function click(button,x,y)
     show_bombs();
   }
 }
-
 function show_bombs()
 {
   for(let x=0;x<rows;x++)
@@ -221,7 +193,6 @@ function show_bombs()
     }
   }
 }
-
 function check_winner()
 {
   let counter=0;
@@ -229,64 +200,67 @@ function check_winner()
   {
     for(let n=0;n<rows;n++)
     {
-      //console.log(grid_container.children[(m)*columns+(n)]);
       if(grid_container.children[(m)*columns+(n)].disabled==false)
       {
         counter++;
-        console.log("ran");
       }
     }
   }
   if(counter== ORIGINAL_number_of_bombs)
   {
-    console.log("won");
     restart_element.textContent=":)";
     game_running = false;
   }
 }
-
-
-
-
-
-
-
-
-
 let time = 0;
-
 function increment_time()
 {
   if(game_running==true)
   {
-  time++;
-  if(time<10)
-  {
-    timer_element.textContent="00"+time;
-  }
-  else if(time>=10 && time<100)
-  {
-    timer_element.textContent="0"+time;
-  }
-  else if(time>=100 && time<1000)
-  {
-    timer_element.textContent=time;
-  }
-  else
-  {
-    timer_element.textContent=999;
-  }
+    time++;
+    if(time<10)
+    {
+      timer_element.textContent="00"+time;
+    }
+    else if(time>=10 && time<100)
+    {
+      timer_element.textContent="0"+time;
+    }
+    else if(time>=100 && time<1000)
+    {
+      timer_element.textContent=time;
+    }
+    else
+    {
+      timer_element.textContent=999;
+    }
   }
 }
-
 setInterval(increment_time,1000);
+restart_element.addEventListener('click',()=>
+{
+  number_of_bombs = 40;
+  displayed_number_of_bombs = 40;
+  grid_of_bombs = [];
+  create_grid();
+  game_running = true;
+  reset_visuals();
+});
 
-
-
-
-
-
-
-
-
-
+function reset_visuals()
+{
+  for(let row_reset=0;row_reset<rows;row_reset++)
+  {
+    for(let column_reset=0;column_reset<columns;column_reset++)
+    {
+      grid_container.children[(row_reset)*columns+(column_reset)].disabled=false;
+      grid_container.children[(row_reset)*columns+(column_reset)].textContent="";
+      grid_container.children[(row_reset)*columns+(column_reset)].style.backgroundColor = '#d9d9d9';
+      grid_container.children[(row_reset)*columns+(column_reset)].style.border = '4px outset black';
+      restart_element.textContent=":|";
+      mines_element.textContent=displayed_number_of_bombs;
+      time = 0;
+      timer_element.textContent="000";
+    }
+  }
+}
